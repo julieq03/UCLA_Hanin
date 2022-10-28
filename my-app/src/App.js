1c1ca4b6d761c6b44a1e1b25d8e10700a1627ab0
Danny <seunghoonlee95@gmail.com>
2022-10-27 23:17:32 -0700

    added navbar but still need to implement submenus under each menu item

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>한인웹사이트 Skeleton</p>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
