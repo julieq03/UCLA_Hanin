d6adcbb51a4510933d95a1ed13f4414c7b5dd07f
Danny <seunghoonlee95@gmail.com>
2022-11-06 10:35:56 -0800

    firebase setup

---
+++ backend/routes/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  // storageBucket: "ucla-hanin.appspot.com",
+  // messagingSenderId: "56072914377",
+  // appId: "1:56072914377:web:da014c7492d60689765418",
+  // measurementId: "G-2FJ8MG472T",
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+export default firebaseApp;
