6b049c1ae551933328370011549c539d8094d3b2
Danny <seunghoonlee95@gmail.com>
2022-11-06 11:16:14 -0800

    Restructured index.js and app.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "./App.css";
-import { Link } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Footer from "./components/Footer";
-import roycehall from "../src/images/roycehall.jpeg"
+import AboutUs from "./pages/AboutUs";
+import Kojobs from "./pages/Kojobs";
+import Kubs from "./pages/Kubs";
+import Ksea from "./pages/Ksea";
+import LoginSignup from "./pages/LoginSignup";
 
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="aboutus" element={<AboutUs />} />
+      <Route path="kclubs/kojobs" element={<Kojobs />} />
+      <Route path="kclubs/kubs" element={<Kubs />} />
+      <Route path="kclubs/ksea" element={<Ksea />} />
+      <Route path="/loginsignup" element={<LoginSignup />} />
+    </Routes>
+  </BrowserRouter>
   );
+
+  
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,26 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { render } from "react-dom";
-import AboutUs from "./pages/AboutUs";
-import Kojobs from "./pages/Kojobs";
-import Kubs from "./pages/Kubs";
-import Ksea from "./pages/Ksea";
-import LoginSignup from "./pages/LoginSignup";
 
-const root = document.getElementById("root");
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="aboutus" element={<AboutUs />} />
-      <Route path="kclubs/kojobs" element={<Kojobs />} />
-      <Route path="kclubs/kubs" element={<Kubs />} />
-      <Route path="kclubs/ksea" element={<Ksea />} />
-      <Route path="/loginsignup" element={<LoginSignup />} />
-    </Routes>
-  </BrowserRouter>,
-  root
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
