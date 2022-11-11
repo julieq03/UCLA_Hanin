af6abdb5d0f0a0becc7bf15cf2618feebcca4a6a
Joonwon (Eric) Lee <joonwontech@gmail.com>
2022-11-10 18:34:02 -0800

    조금 손댐

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -10,10 +10,7 @@ function Home() {
       <Navbar />
       {/* <h1 className="homepageBannerTitle">UCLA Koreans</h1>
       <img src={roycehall} alt="homepageBanner" className="homepageBannerImg" /> */}
-      <div
-        className="homepageBanner"
-        style={{ backgroundImage: `url(${roycehall})` }}
-      >
+      <div className="homepageBanner" style={{ backgroundImage: `url(${roycehall})` }}>
         <div className="homepageBannerTitle">
           <h1>UCLA Koreans Association</h1>
           <NavLink to="/aboutus" className="linkmenuItem">
@@ -21,9 +18,6 @@ function Home() {
           </NavLink>
         </div>
       </div>
-      <h1>Contents 뭐들어가야하지1</h1>
-      <h1>Contents 뭐들어가야하지2</h1>
-      <h1>Contents 뭐들어가야하지3</h1>
       <Footer />
     </React.Fragment>
   );
